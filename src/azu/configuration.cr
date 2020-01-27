723b6eee9552e7d66da2df8047916299cf1ccd02
Elias J. Perez <elias.perez@nytimes.com>
2020-01-27 18:36:47 -0500

    Use environment variables

--- src/azu/configuration.cr
+++ src/azu/configuration.cr
@@ -1,11 +1,22 @@
+require "logger"
+
 module Azu
   class Configuration
-    property port : Int32 = 4000
-    property port_reuse : Bool = true
-    property host : String = "0.0.0.0"
-    property log : Logger = Logger.new(STDOUT)
-    property env : String = "development"
+    property port : Int32 = ENV.fetch("PORT", "4000").to_i
+    property port_reuse : Bool = ENV.fetch("PORT_REUSE", "false") == "true"
+    property host : String = ENV.fetch("HOST", "0.0.0.0")
+    property log : Logger
+    property env : String = ENV.fetch("AZU_ENV", "development")
     property router : Router = Router.new
     property pipelines : Pipeline = Pipeline.new
+
+    def initialize
+      @log = ::Logger.new(writer)
+    end
+
+    private def writer
+      file = File.new("#{env}.log", "a")
+      IO::MultiWriter.new(file, STDOUT)
+    end
   end
 end
