7a4e48a280f92b1843242d42ec364065b7fd6497
Elias J. Perez <eliasjpr@gmail.com>
2020-01-27 19:17:02 -0500

    Removes unused methods

--- src/azu/handlers/render.cr
+++ src/azu/handlers/render.cr
@@ -7,14 +7,5 @@ module Azu
       _endpoint, endpoint = route.payload
       context.response.output << endpoint.new(context, route.params).call
     end
-
-    protected def path(context)
-      upgrade_path(context) + context.request.method.downcase + context.request.path.rstrip('/')
-    end
-
-    protected def upgrade_path(context)
-      return "/ws" if context.request.headers.includes_word?("Upgrade", "Websocket")
-      "/"
-    end
   end
 end
