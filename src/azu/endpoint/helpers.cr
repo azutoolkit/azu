27370ca52b885f59c3fc2eb8ed73dfcc7ba40833
Elias J. Perez <eliasjpr@gmail.com>
2020-01-30 20:38:47 -0500

    Cleans up endpoint helpers

--- src/azu/endpoint/helpers.cr
+++ src/azu/endpoint/helpers.cr
@@ -1,35 +1,30 @@
 module Helpers
-  private def method
+  def method
     Method.parse(context.request.method)
   end
 
-  private def headers
+  def header
     context.request.headers
   end
 
-  private def headers(key : String, value : String)
+  def header(key : String, value : String)
     context.response.headers[key] = value
   end
 
-  private def redirect(to : String, status : Int32)
+  def redirect(to locaton : String, status : Int32)
+    status status
+    header location
   end
 
-  private def redirect(to : Endpoint, status : Int32)
-  end
-
-  private def cookies
+  def cookies
     context.request.cookies
   end
 
-  private def cookies(cookie : HTTP::Cookie)
+  def cookies(cookie : HTTP::Cookie)
     context.response.cookies << cookie
   end
 
-  private def status(status : HTTP::Status)
-    context.response.status = status
-  end
-
-  private def status(status : Int32)
-    context.response.status = HTTP::Status.new(status_code)
+  def status(status : Int32)
+    context.response.status_code = status
   end
 end
