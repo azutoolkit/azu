a78f3916014c3988a8af53f828d860f62a8807a7
Elias J. Perez <elias.perez@nytimes.com>
2020-02-04 09:19:38 -0500

    Fixes running specs

--- spec/azu_spec.cr
+++ spec/azu_spec.cr
@@ -2,7 +2,7 @@ require "./spec_helper.cr"
 require "http/client"
 
 describe Azu do
-  client = HTTP::Client.new Azu.config.host, Azu.config.port
+  client = HTTP::Client.new "localhost", Azu.config.port
 
   describe "Http Errors" do
     it "returns request not found" do
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -3,12 +3,10 @@ require "../src/azu"
 
 ENV.fetch "CRYSTAL_ENV", "testing"
 
-process = nil
+process = Process.new("./bin/test", shell: true)
+sleep 1
+client = HTTP::Client.new "localhost", Azu.config.port
 
-Spec.before_suite do
-  process = Process.new("./bin/test")
-  sleep 15
-end
 
 Spec.after_suite do
   process.not_nil!.kill
