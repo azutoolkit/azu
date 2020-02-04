07ff32135b5e222fabdf1d8dd9354f5457443c7c
Elias J. Perez <elias.perez@nytimes.com>
2020-02-04 09:19:50 -0500

    Fixes running specs

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -5,8 +5,6 @@ ENV.fetch "CRYSTAL_ENV", "testing"
 
 process = Process.new("./bin/test", shell: true)
 sleep 1
-client = HTTP::Client.new "localhost", Azu.config.port
-
 
 Spec.after_suite do
   process.not_nil!.kill
