181eac7eb2a2677a10ba0af205de6a1c52a6d17e
Elias J. Perez <eliasjpr@gmail.com>
2020-08-10 15:30:49 -0400

    fixes js formatting

--- src/azu/js/live-view.js
+++ src/azu/js/live-view.js
@@ -6,10 +6,10 @@
   url.protocol = url.protocol.replace('http', 'ws');
   url.pathname = '/live-view';
   var live_view = new WebSocket(url);
-  live_view.addEventListener('open', event => {
+  live_view.addEventListener('open', (event) => {
     // Hydrate client-side rendering
     document.querySelectorAll('[data-live-view]')
-      .forEach(view => {
+      .forEach((view)=> {
         var node = html(view.innerHTML)[0];
         preact.render(node, view, view.children[0]);
         live_view.send(JSON.stringify({
@@ -18,17 +18,17 @@
       });
   });
 
-  live_view.addEventListener('message', event => {
+  live_view.addEventListener('message', (event) => {
     var data = event.data;
     var { id, render } = JSON.parse(data);
 
     document.querySelectorAll(`[data-live-view="${id}"]`)
-      .forEach(view => {
+      .forEach((view) => {
         preact.render(html('<div>' + render + '</div>')[0], view, view.children[0]);
       });
   });
 
-  live_view.addEventListener('close', event => {
+  live_view.addEventListener('close', (event) => {
     // Do we need to do anything here?
   });
 
@@ -36,8 +36,8 @@
     'click',
     'change',
     'input',
-  ].forEach(event_type => {
-    document.addEventListener(event_type, event => {
+  ].forEach((event_type) => {
+    document.addEventListener(event_type, (event) => {
       var element = event.target;
       var event_name = element.getAttribute('live-' + event_type);
 
