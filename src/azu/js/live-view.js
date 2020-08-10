b24b1387c72eeffc8ba027699c0816f7e09e4b0a
Elias J. Perez <eliasjpr@gmail.com>
2020-08-10 16:11:01 -0400

    Fix js formatting

--- src/azu/js/live-view.js
+++ src/azu/js/live-view.js
@@ -3,55 +3,83 @@
   var html = converter.convert;
 
   var url = new URL(location.href);
-  url.protocol = url.protocol.replace('http', 'ws');
-  url.pathname = '/live-view';
+  url.protocol = url.protocol.replace("http", "ws");
+  url.pathname = "/live-view";
   var live_view = new WebSocket(url);
+<<<<<<< HEAD
   live_view.addEventListener('open', (event) => {
     // Hydrate client-side rendering
     document.querySelectorAll('[data-live-view]')
       .forEach((view)=> {
+=======
+  live_view.addEventListener("open", event => {
+    // Hydrate client-side rendering
+    document.querySelectorAll("[data-live-view]")
+      .forEach(view => {
+>>>>>>> Fix js formatting
         var node = html(view.innerHTML)[0];
         preact.render(node, view, view.children[0]);
         live_view.send(JSON.stringify({
-          subscribe: view.getAttribute('data-live-view'),
+          subscribe: view.getAttribute("data-live-view"),
         }))
       });
   });
 
+<<<<<<< HEAD
   live_view.addEventListener('message', (event) => {
+=======
+  live_view.addEventListener("message", event => {
+>>>>>>> Fix js formatting
     var data = event.data;
     var { id, render } = JSON.parse(data);
 
     document.querySelectorAll(`[data-live-view="${id}"]`)
+<<<<<<< HEAD
       .forEach((view) => {
         preact.render(html('<div>' + render + '</div>')[0], view, view.children[0]);
       });
   });
 
   live_view.addEventListener('close', (event) => {
+=======
+      .forEach(view => {
+        preact.render(html("<div>" + render + "</div>")[0], view, view.children[0]);
+      });
+  });
+
+  live_view.addEventListener("close", event => {
+>>>>>>> Fix js formatting
     // Do we need to do anything here?
   });
 
   [
+<<<<<<< HEAD
     'click',
     'change',
     'input',
   ].forEach((event_type) => {
     document.addEventListener(event_type, (event) => {
+=======
+    "click",
+    "change",
+    "input",
+  ].forEach(event_type => {
+    document.addEventListener(event_type, event => {
+>>>>>>> Fix js formatting
       var element = event.target;
-      var event_name = element.getAttribute('live-' + event_type);
+      var event_name = element.getAttribute("live-" + event_type);
 
-      if(typeof event_name === 'string') {
+      if(typeof event_name === "string") {
         var channel = event
           .target
-          .closest('[data-live-view]')
-          .getAttribute('data-live-view')
+          .closest("[data-live-view]")
+          .getAttribute("data-live-view")
 
         var data = {};
         switch(element.type) {
           case "checkbox": data = { value: element.checked }; break;
           // Are there others?
-          default: data = { value: element.getAttribute('live-value') || element.value }; break;
+          default: data = { value: element.getAttribute("live-value") || element.value }; break;
         }
 
         live_view.send(JSON.stringify({
