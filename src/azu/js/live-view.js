6b68ad8e3e3d21a49173ca4fa212fff71dbebf6b
Elias J. Perez <eliasjpr@gmail.com>
2020-09-02 07:44:36 -0400

    Removes Javascript Tag in favor of javascript module

--- src/azu/js/live-view.js
+++ src/azu/js/live-view.js
@@ -1,65 +1,72 @@
-(() => {
-  var converter = preactHtmlConverter.PreactHTMLConverter();
-  var html = converter.convert;
-  var url = new URL(location.href);
+import { h, Component, render, hydrate} from 'https://unpkg.com/preact?module';
+import htm from 'https://unpkg.com/htm?module';
+const html = htm.bind(h);
 
-  url.protocol = url.protocol.replace("http", "ws");
-  url.pathname = "/live-view";
+var url = new URL(location.href);
+url.protocol = url.protocol.replace('http', 'ws');
+url.pathname = '/spark-view';
+var live_view = new WebSocket(url);
 
-  var live_view = new WebSocket(url);
+const sparkRenderEvent = new CustomEvent('spark-render');
 
-  live_view.addEventListener("open", (event) => {
-    document.querySelectorAll("[data-live-view]")
-      .forEach((view)=> {
-        var node = html(view.innerHTML)[0];
-        preact.render(node, view, view.children[0]);
-        live_view.send(JSON.stringify({
-          subscribe: view.getAttribute("data-live-view"),
-        }))
-      });
-  });
+live_view.addEventListener('open', (event) => {
+  // Hydrate client-side rendering
+  document.querySelectorAll('[data-live-view]')
+    .forEach((view)=> {
+      var node = html(view.innerHTML)[0];
+      hydrate(node, view.children[0]);
 
-  live_view.addEventListener("message", (event) => {
-    var data = event.data;
-    var { id, render } = JSON.parse(data);
-
-    document.querySelectorAll(`[data-live-view="${id}"]`)
-      .forEach((view) => {
-        preact.render(html("<div>" + render + "</div>")[0], view, view.children[0]);
-      });
-  });
-
-  live_view.addEventListener("close", (event) => {
-    // Do we need to do anything here?
-  });
-
-  [
-    "click",
-    "change",
-    "input",
-  ].forEach((event_type) => {
-    document.addEventListener(event_type, (event) => {
-      var element = event.target;
-      var event_name = element.getAttribute("live-" + event_type);
-
-      if(typeof event_name === "string") {
-        var channel = event
-          .target
-          .closest("[data-live-view]")
-          .getAttribute("data-live-view")
-
-        var data = {};
-        switch(element.type) {
-          case "checkbox": data = { value: element.checked }; break;
-          default: data = { value: element.getAttribute("live-value") || element.value }; break;
-        }
-
-        live_view.send(JSON.stringify({
-          event: event_name,
-          data: JSON.stringify(data),
-          channel: channel,
-        }));
-      }
+      live_view.send(JSON.stringify({
+        subscribe: view.getAttribute('data-live-view'),
+      }))
     });
+});
+
+live_view.addEventListener('message', (event) => {
+  var html = htm.bind(h);
+  var data = event.data;
+  var { id, content } = JSON.parse(data);
+  document.querySelectorAll(`[data-live-view="${id}"]`)
+    .forEach((view) => {
+      var div = window.$('<div>' + content + '</div>');
+      view.children[0].innerHTML = div[0].innerHTML
+      render(div[0], view, view.children[0]) ;
+  
+      document.dispatchEvent(sparkRenderEvent);
+    });
+});
+
+live_view.addEventListener('close', (event) => {
+  // Do we need to do anything here?
+});
+
+[
+  'click',
+  'change',
+  'input',
+].forEach((event_type) => {
+  document.addEventListener(event_type, (event) => {
+    var element = event.target;
+    var event_name = element.getAttribute('live-' + event_type);
+
+    if(typeof event_name === 'string') {
+      var channel = event
+        .target
+        .closest('[data-live-view]')
+        .getAttribute('data-live-view')
+
+      var data = {};
+      switch(element.type) {
+        case "checkbox": data = { value: element.checked }; break;
+        // Are there others?
+        default: data = { value: element.getAttribute('live-value') || element.value }; break;
+      }
+
+      live_view.send(JSON.stringify({
+        event: event_name,
+        data: JSON.stringify(data),
+        channel: channel,
+      }));
+    }
   });
-})();
+});
--- src/azu/spark.cr
+++ src/azu/spark.cr
@@ -2,85 +2,6 @@ require "uuid"
 
 module Azu
   class Spark < Channel
-    def self.javascript_tags
-      <<-HTML
-        <script type="module">
-          import { h, Component, render, hydrate} from 'https://unpkg.com/preact?module';
-          import htm from 'https://unpkg.com/htm?module';
-          const html = htm.bind(h);
-
-          var url = new URL(location.href);
-          url.protocol = url.protocol.replace('http', 'ws');
-          url.pathname = '/live-view';
-          var live_view = new WebSocket(url);
-
-          const sparkRenderEvent = new CustomEvent('spark-render');
-
-          live_view.addEventListener('open', (event) => {
-            // Hydrate client-side rendering
-            document.querySelectorAll('[data-live-view]')
-              .forEach((view)=> {
-                var node = html(view.innerHTML)[0];
-                hydrate(node, view.children[0]);
-
-                live_view.send(JSON.stringify({
-                  subscribe: view.getAttribute('data-live-view'),
-                }))
-              });
-          });
-
-          live_view.addEventListener('message', (event) => {
-            var html = htm.bind(h);
-            var data = event.data;
-            var { id, content } = JSON.parse(data);
-            document.querySelectorAll(`[data-live-view="${id}"]`)
-              .forEach((view) => {
-                var div = window.$('<div>' + content + '</div>');
-                view.children[0].innerHTML = div[0].innerHTML
-                render(div[0], view, view.children[0]) ;
-            
-                document.dispatchEvent(sparkRenderEvent);
-              });
-          });
-
-          live_view.addEventListener('close', (event) => {
-            // Do we need to do anything here?
-          });
-
-          [
-            'click',
-            'change',
-            'input',
-          ].forEach((event_type) => {
-            document.addEventListener(event_type, (event) => {
-              var element = event.target;
-              var event_name = element.getAttribute('live-' + event_type);
-
-              if(typeof event_name === 'string') {
-                var channel = event
-                  .target
-                  .closest('[data-live-view]')
-                  .getAttribute('data-live-view')
-
-                var data = {};
-                switch(element.type) {
-                  case "checkbox": data = { value: element.checked }; break;
-                  // Are there others?
-                  default: data = { value: element.getAttribute('live-value') || element.value }; break;
-                }
-
-                live_view.send(JSON.stringify({
-                  event: event_name,
-                  data: JSON.stringify(data),
-                  channel: channel,
-                }));
-              }
-            });
-          });
-        </script>   
-      HTML
-    end
-
     COMPONENTS = {} of String => SparkView
 
     def on_binary(binary); end
@@ -140,7 +61,7 @@ module Azu
     end
 
     def refresh
-      json = {content: component.to_s, id: spark_id}.to_json
+      json = {content: to_s, id: spark_id}.to_json
       @socket.not_nil!.send json
     end
 
