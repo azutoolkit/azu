46557e2373fe66f14a194a36ccc245ee6f7061e3
Elias J. Perez <eliasjpr@gmail.com>
2020-10-03 19:24:11 -0400

    Adds endpoint documentation

--- src/azu/endpoint.cr
+++ src/azu/endpoint.cr
@@ -1,22 +1,50 @@
 require "./helpers"
 
 module Azu
-  module Endpoint(R, V)
+  # Defines a Azu endpoint.
+  # The endpoint is the final stage of the request process
+  # Each endpoint is the location from which APIs can access the resources of your application
+  # to carry out their function.
+  #
+  # Azu endpoints is a simple module that defines the request and response object
+  # to execute your application business domain logic. Endpoints specify where resources can be
+  # accessed by APIs and the key role is to guarantee the correct functioning of the calls.
+  #
+  # ## Correctness
+  #
+  # To ensure correctness Azu Endpoints are design with the Request and Response pattern in mind
+  # you can think of it as input and output to a function, where the request is the input and the
+  # response is the output.
+  #
+  # Request and Response objects are type safe objects that can be designed by contract.
+  # Read more about `Azu::Contract`
+  #
+  # ```
+  # module ExampleApp
+  #   class UserEndpoint
+  #     include Azu::Endpoint(UserRequest, UserResponse)
+  #   end
+  # end
+  # ```
+  #
+  module Endpoint(Request, Response)
     include HTTP::Handler
     include Helpers
 
     @context = uninitialized HTTP::Server::Context
 
-    abstract def call : V
+    abstract def call : Response
 
-    def call(context : HTTP::Server::Context)
+    # :nodoc:
+    protected def call(context : HTTP::Server::Context)
       @context = context
       ContentNegotiator.content @context, call
       @context
     end
 
+    # :nodoc:
     private def request
-      R.new @context
+      Request.new @context
     end
   end
 end
