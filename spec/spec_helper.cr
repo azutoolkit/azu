ee7ed3f1edab13839c206b27e1d62e6b7ddc0e6a
Elias J. Perez <eliasjpr@gmail.com>
2020-10-06 21:03:56 -0400

    Fix specs

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,12 +1,13 @@
 require "spec"
 require "../src/azu"
 
-ENV["CRYSTAL_ENV"] ||= "testing"
+ENV["CRYSTAL_ENV"] ||= "test"
 
 process = Process.new("./bin/example_app")
 # Wait for process to start
-sleep 1.seconds
+sleep 2.seconds
 
 Spec.after_suite do
+  sleep 1.seconds
   process.not_nil!.signal Signal::KILL
 end
