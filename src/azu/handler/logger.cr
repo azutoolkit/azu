70df97fda99ca02a8d741d8892cafc225133b366
Elias Perez <eliasjpr@users.noreply.github.com>
2022-05-10 09:23:20 -0400

    Simpler Log Handler
    Why
    The current output is overwhelming and does not follow standards

--- src/azu/handler/logger.cr
+++ src/azu/handler/logger.cr
@@ -11,66 +11,59 @@ module Azu
       end
 
       def call(context : HTTP::Server::Context)
-        call_next(context)
-        spawn do
-          log.info { message(context) }
+        start = Time.monotonic
+
+        begin
+          call_next(context)
+        ensure
+          elapsed = Time.monotonic - start
+          elapsed_text = elapsed(elapsed)
+
+          req = context.request
+          res = context.response
+
+          addr =
+            case remote_address = req.remote_address
+            when nil
+              "-"
+            when Socket::IPAddress
+              remote_address.address
+            else
+              remote_address
+            end
+
+          @log.info { message(addr, req, res, elapsed_text) }
         end
-        context
       end
 
-      private def message(context)
-        time = Time.monotonic
+      private def message(addr, req, res, elapsed_text)
         String.build do |str|
-          str << "HTTP Request".colorize(:green).underline
-          str << entry(:Method, context.request.method, :green)
-          str << entry(:Path, context.request.resource, :light_blue)
-          str << status_entry(:Status, http_status(context.response.status_code))
-          str << entry(:Latency, elapsed(Time.monotonic - time), :green)
-
-          format_hash("Headers", context.request.headers, str)
-          format_hash("Query Params", context.request.query_params, str)
-          format_hash("Path Params", context.request.path_params, str)
+          str << addr.colorize(:green).underline
+          str << entry(:Address, addr, :green)
+          str << entry(:Method, req.method, :green)
+          str << entry(:Path, req.resource, :light_blue)
+          str << status(:Status, http_status(res.status_code))
+          str << entry(:Latency, elapsed_text, :green)
         end
-      end
-
-      private def format_hash(title, h, str)
-        str << "\n\n"
-        str << "  #{title}".colorize(:light_gray).bold.underline
-
-        h.map do |k, v|
-          str << "\n"
-          str << "   ⤑  ".colorize(:light_gray)
-          str << "#{k}: ".colorize(:white)
-          str << case v
-          when Hash  then format_hash("Sub", v, str)
-          when Array then v.join(", ").colorize(:cyan)
-          else            v.colorize(:cyan)
-          end
-        end
-
-        str
       end
 
       private def elapsed(elapsed)
         millis = elapsed.total_milliseconds
         return "#{millis.round(2)}ms" if millis >= 1
-
         "#{(millis * 1000).round(2)}µs"
       end
 
       private def entry(key, message, color)
         String.build do |str|
-          str << "\n"
-          str << "   ⤑  ".colorize(:green)
+          str << " ⤑ ".colorize(:green)
           str << "#{key}: ".colorize(:white)
           str << message.colorize(color)
         end
       end
 
-      private def status_entry(key, message)
+      private def status(key, message)
         String.build do |str|
-          str << "\n"
-          str << "   ⤑  ".colorize(:green)
+          str << " ⤑ ".colorize(:green)
           str << "#{key}: ".colorize(:white)
           str << message
         end
@@ -82,8 +75,7 @@ module Azu
         when 300..399 then status.colorize(:blue)
         when 400..499 then status.colorize(:yellow)
         when 500..599 then status.colorize(:red)
-        else
-          status.colorize(:white)
+        else               status.colorize(:white)
         end
       end
     end
